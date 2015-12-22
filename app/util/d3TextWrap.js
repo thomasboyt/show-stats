a772b03b6dd041b34b4c363103124667f1f82dfe
Thomas Boyt <me@thomasboyt.com>
2015-12-22 03:07:41 -0500

    Rewrite d3TextWrap to use dy correctly (??)

--- app/util/d3TextWrap.js
+++ app/util/d3TextWrap.js
@@ -2,24 +2,38 @@ import d3 from 'd3';
 
 export default function wrap(text, width) {
   text.each(function() {
-    var text = d3.select(this),
-        words = text.text().split(/\s+/).reverse(),
-        word,
-        line = [],
-        lineNumber = 0,
-        lineHeight = 1.1, // ems
-        y = text.attr("y"),
-        dy = parseFloat(text.attr("dy")),
-        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-    while (word = words.pop()) {
+    const text = d3.select(this);
+    const words = text.text().split(/\s+/);
+    const y = text.attr('y');
+    const dy = parseFloat(text.attr('dy')) || 0;
+
+    const lineHeight = 1.1;  // ems
+
+    let tspan = text.text(null).append('tspan')
+      .attr('x', 0)
+      .attr('y', y)
+      .attr('dy', `${dy}em`);
+
+    let line = [];
+    let lineNumber = 0;
+
+    words.forEach((word) => {
       line.push(word);
-      tspan.text(line.join(" "));
+
+      tspan.text(line.join(' '));
+
       if (tspan.node().getComputedTextLength() > width) {
         line.pop();
-        tspan.text(line.join(" "));
+        tspan.text(line.join(' '));
         line = [word];
-        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+        lineNumber += 1;
+
+        tspan = text.append('tspan')
+          .attr('x', 0)
+          .attr('y', y)
+          .attr('dy', `${lineHeight + dy}em`)
+          .text(word);
       }
-    }
+    });
   });
 }
